4fcfea2de5dca1e5849264c0c02eda194567f503
Sebastian1999 <137645250+olakases07@users.noreply.github.com>
2024-02-19 16:55:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Bienvenidos a C'Natural");
+alert("Proyecto formativo de Carlos Eduardo Hernández Piedrahita y Sebastian Andres Caldera");
+
+function showCustomAlert() {
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'block';
+}
+
+function closeCustomAlert() {
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'none';
+}
